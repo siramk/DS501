47a7a2b6df9a913d513ef2bf08c7a85355bccfcd
siramNIkhil2000 <siram.kumar@needl.ai>
2021-02-17 20:21:51 +0530

    1.) default port of server added

--- src/NetflixAPI/app/views.py
+++ src/NetflixAPI/app/views.py
@@ -23,4 +23,4 @@ app.add_url_rule('/genre_match_endpoint/<string:boolean_query>', 'genre_match_en
    
 if __name__ == '__main__': 
 
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
